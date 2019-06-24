3149152c3d82641abf5d20126d4d85cf2fc050f3
İzzet Kara <izzet26kara@gmail.com>
2019-06-24 18:52:47 +0300

    Commit second

---
+++ github2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  github2
+//
+//  Created by İzzet Kara on 24.06.2019.
+//  Copyright © 2019 Izzet Kara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("hi world!")
+    }
+
+
+}
+
